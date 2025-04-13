5785c1f4d554a24d63dc651be2a471a3d59b1b4e
Ehud Melnik <EFMELNIK@email.sc.edu>
2025-04-12 23:09:31 -0400

    script.js

--- JS Blog JavaScript/script.js
+++ JS Blog JavaScript/script.js
@@ -1,23 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Social Media Sharing Functions
+    const urlToShare = 'https://efmelnik.github.io/cooking-delights/index.html';
+
     function shareOnFacebook() {
-    // Replace 'your-website-url' with the URL you want to share
-    const urlToShare = 'https://www.example.com';
-    const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(urlToShare)}`;
-    window.open(facebookShareUrl, 'Share on Facebook', 'width=600,height=400');
-    }
-    
-    function shareOnTwitter() {
-    // Replace 'your-website-url' and 'your-text' with your desired URL and text
-    const urlToShare = 'your-website-url';
-    const textToShare = 'your-text';
-    const twitterShareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(urlToShare)}&text=${encodeURIComponent(textToShare)}`;
-    window.open(twitterShareUrl, 'Share on Twitter', 'width=600,height=400');
+        const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(urlToShare)}`;
+        window.open(facebookShareUrl, 'Share on Facebook', 'width=600,height=400');
     }
 
-    // Event listeners for social media sharing buttons
+    function shareOnTwitter() {
+        const textToShare = 'Check Out The Cooking Delights Website!';
+        const twitterShareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(urlToShare)}&text=${encodeURIComponent(textToShare)}`;
+        window.open(twitterShareUrl, 'Share on Twitter', 'width=600,height=400');
+    }
+
     const shareFacebookButton = document.getElementById("share-facebook");
     const shareTwitterButton = document.getElementById("share-twitter");
-    shareFacebookButton.addEventListener("click", shareOnFacebook);
-    shareTwitterButton.addEventListener("click", shareOnTwitter);
-    })
+
+    if (shareFacebookButton) {
+        shareFacebookButton.addEventListener("click", shareOnFacebook);
+    }
+
+    if (shareTwitterButton) {
+        shareTwitterButton.addEventListener("click", shareOnTwitter);
+    }
+});
