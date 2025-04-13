6e8e2e24fdaf4d8bbe5dcddd5d0497c3ebc2a01e
Ehud Melnik <EFMELNIK@email.sc.edu>
2025-04-12 23:17:52 -0400

    script.js

--- JS Blog JavaScript/script.js
+++ JS Blog JavaScript/script.js
@@ -1,12 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const urlToShare = 'https://efmelnik.github.io/cooking-delights/index.html';
 
     function shareOnFacebook() {
+        const urlToShare = 'https://efmelnik.github.io/cooking-delights/index.html';
         const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(urlToShare)}`;
         window.open(facebookShareUrl, 'Share on Facebook', 'width=600,height=400');
     }
 
     function shareOnTwitter() {
+        const urlToShare = 'https://efmelnik.github.io/cooking-delights/index.html';
         const textToShare = 'Check Out The Cooking Delights Website!';
         const twitterShareUrl = `https://twitter.com/intent/tweet?url=${encodeURIComponent(urlToShare)}&text=${encodeURIComponent(textToShare)}`;
         window.open(twitterShareUrl, 'Share on Twitter', 'width=600,height=400');
